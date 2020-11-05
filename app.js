344ad25289c20b346baa5c2a13aa8ab0b5ae352b
Sreejith V S <sreejithvs333@gmail.com>
2020-11-05 11:51:17 +0530

    code refactoring

--- app.js
+++ app.js
@@ -1,66 +1,66 @@
-    // Parent Animal constructor
-    class Animal {
-        constructor(name, height, weight, diet){
-            this.name = name;
-            this.name = height;
-            this.weight = weight;
-            this.diet = diet;
-        }
+// Parent Animal class
+class Animal {
+    constructor(name, height, weight, diet) {
+        this.name = name;
+        this.name = height;
+        this.weight = weight;
+        this.diet = diet;
     }
-    // Create Dino Constructor
-    class Dino extends Animal {
-        constructor(name, height, weight, diet, species, where, when, fact){
-            super(name, height, weight, diet);
-            this.species = species;
-            this.where = where;
-            this.when = when;
-            this.fact = fact;
-        }
+}
+// Child Dino class
+class Dino extends Animal {
+    constructor(name, height, weight, diet, species, where, when, fact) {
+        super(name, height, weight, diet);
+        this.species = species;
+        this.where = where;
+        this.when = when;
+        this.fact = fact;
     }
-    // Create Dino Objects
-    let dinoArray = [];
-    fetch("./dino.json").then(res=>{
-        if(res.ok) {
-            return res.json()
-        }else {
-            throw new Error('Oops! Something went wrong! Please try again.');
-        }
-    }).then(data=>{
-        //TODO: remove log
-        console.log(data);
-        dinoArray = [...data.Dinos];
-        console.log(dinoArray);
-    }).catch(error=>{
-        console.log(error);
-    });
+}
+// Create Dino Objects
+let dinoArray = [];
+fetch("./dino.json").then(res => {
+    if (res.ok) {
+        return res.json()
+    } else {
+        throw new Error('Oops! Something went wrong! Please try again.');
+    }
+}).then(data => {
+    //TODO: remove log
+    console.log(data);
+    dinoArray = [...data.Dinos];
+    console.log(dinoArray);
+}).catch(error => {
+    console.log(error);
+});
 
-    // Create Human Object
+// Create Human Object
 
-    // Use IIFE to get human data from form
-
-
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Use IIFE to get human data from form
 
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches. 
 
-    // Remove form from screen
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Generate Tiles for each Dino in Array
+
+// Add tiles to DOM
+
+// Remove form from screen
 
 
 // On button click, prepare and display infographic
 const button = document.getElementById('btn');
-button.addEventListener("click", event=>{
+button.addEventListener("click", event => {
     console.log("button clicked!");
     //TODO: Hide form & display grid
 })
